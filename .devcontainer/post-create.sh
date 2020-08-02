8db84c099fd5799468c3af3237e423587847c85a
chinthakaru <chinthakaru@gmail.com>
2020-08-02 15:29:55 +1000

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,5 @@
 # Install Terraform
-wget https://releases.hashicorp.com/terraform/0.12.23/terraform_0.12.23_linux_amd64.zip
-unzip terraform_0.12.23_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.12.29/terraform_0.12.29_linux_amd64.zip
+unzip terraform_0.12.29_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
-rm terraform_0.12.23_linux_amd64.zip
+rm terraform_0.12.29_linux_amd64.zip
